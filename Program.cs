8f139a8d090742d658c0aa11f67155528b9f0460
Tak1wa <iwasa.takahito@classmethod.jp>
2025-06-03 05:30:42 +0900

    モジュールインジェクションセキュリティの非準拠

--- Program.cs
+++ Program.cs
@@ -9,19 +9,23 @@ namespace HelloWorld
         {
             // 標準出力に "Hello World!" を表示
             Console.WriteLine("Hello World!");
-            
+
             // 標準入力からの読み取りを示すメッセージ
             Console.WriteLine("あなたの名前を入力してください (Please enter your name):");
-            
+
             // 標準入力から名前を読み取る
             string? name = Console.ReadLine();
-            
+
             // 入力された名前を使って挨拶を表示
             Console.WriteLine($"こんにちは、{name}さん！ (Hello, {name}!)");
-            
+
             // キー入力を待つ
             Console.WriteLine("終了するには何かキーを押してください (Press any key to exit)...");
             Console.ReadKey();
+
+            //モジュールインジェクションセキュリティの非準拠
+            var listener = new System.Net.HttpListener();
+            listener.Prefixes.Add("http://*:8443/");
         }
     }
 }
